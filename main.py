efbab52356bf06fcef8f24e562caa89c717812a7
annasybingco <sybingcoanna@gmail.com>
2025-04-17 01:07:34 -0400

    learned about crud operators, create collection for api for creating post and getting single posts

--- main.py
+++ main.py
@@ -1,12 +1,23 @@
 from fastapi import FastAPI
 from fastapi.params import Body
 from pydantic import BaseModel
+from typing import Optional
+from random import randrange
 
 app = FastAPI()
 
 class Post(BaseModel):
     title: str
     content: str
+    published: bool = True
+    rating: Optional[int] = None
+
+my_posts = [{"title":"title of post 1", "content":"content of post 1","id":1}, {"title":"fav foods", "content": "I like pizza", "id":2}]
+
+def find_post(id):
+    for p in my_posts:
+        if p["id"] == id:
+            return p
 
 @app.get("/")
 def root():
@@ -14,9 +25,17 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return{"data" : "This is your post"}
+    return{"data" : my_posts}
 
-@app.post("/createposts")
-def create_posts(new_post: Post):
-    print(new_post)
-    return{"data":"new post"}
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(0, 10000)
+    my_posts.append(post_dict)
+    return{"data":post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    post = find_post(id)
+    print(post)
+    return {"post_detail": post}
